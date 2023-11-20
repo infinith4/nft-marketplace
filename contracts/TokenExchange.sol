d83f4c8b522ab24ab94ac8b491593cd3766586c2
infinith4 <infinith4@gmail.com>
2023-11-20 22:18:48 +0900

    Update TokenExchange.sol

--- contracts/TokenExchange.sol
+++ contracts/TokenExchange.sol
@@ -2,24 +2,24 @@
 pragma solidity ^0.8.20;
 
 contract TokenExchange {
-  // token name
+  /// @dev token name
   string private _name;
-  // token symbol
+  /// @dev token symbol
   string private _symbol;
 
-  //token all amount
+  /// @dev token all amount
   uint256 constant _totalSupply = 1000;
 
-  // token exchange total deposit
+  /// @dev token exchange total deposit
   uint256 private _exchangeTotalDeposit;
   
-  // token exchange owner address
+  /// @dev token exchange owner address
   address public owner;
 
-  // token balances each of account
+  /// @dev token balances each of account
   mapping(address => uint256) private _balances;
 
-  // token exchange balances
+  /// @dev token exchange balances
   mapping(address => uint256) private _tokeExchangeBalances;
 
 }
