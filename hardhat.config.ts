71386be9645ded5f56c2bb0fa235a47573bdd96d
infinith4 <infinith4@gmail.com>
2023-11-20 18:18:36 +0900

    add hardhat-verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'dotenv/config'
+import "@nomicfoundation/hardhat-verify";
 
 const privateKey = process.env.PRIVATE_KEY || "";
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
@@ -10,10 +12,18 @@ const config: HardhatUserConfig = {
   networks:{
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`${privateKey}`],
-      gasPrice: 1000000000,
+      accounts: [`${privateKey}`]
     }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: `${etherscanApiKey}`
+    }
+  },
+  sourcify: {
+    enabled: true
   }
+
 };
 
 export default config;
