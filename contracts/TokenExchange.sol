ffffd4ddbf658579cc2b2868480ae806e42bb2d2
infinith4 <infinith4@gmail.com>
2023-11-21 22:03:26 +0900

    constructor, get value function

--- contracts/TokenExchange.sol
+++ contracts/TokenExchange.sol
@@ -30,4 +30,25 @@ contract TokenExchange {
 
   /// @dev Withdraw token
   event TokenWithdraw(address indexed from, uint256 amount);
+
+  constructor (string memory name_, string memory symbol_) {
+    _name = name_;
+    _symbol = symbol_;
+    owner = msg.sender;
+    _balances[owner] = _totalSupply;
+  }
+
+  /// @dev return token name
+  function name() public view returns (string memory){
+    return _name;
+  }
+
+  /// @dev return token totalSupply
+  function totalSupply() public pure returns (uint256){
+    return _totalSupply;
+  }
+  /// @dev return token balance of account address
+  function balanceOf(address account) public view returns (uint256){
+    return _balances[account];
+  }
 }
