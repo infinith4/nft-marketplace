920208f95ebadc5943d8eaadfc27340ffeb9c40e
infinith4 <infinith4@gmail.com>
2023-11-27 00:00:20 +0900

    Update TokenExchange.ts

--- test/TokenExchange.ts
+++ test/TokenExchange.ts
@@ -22,7 +22,7 @@ async function deployOneYearTokenExchangeFixture(isTransfer: boolean = false) {
   const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
   // Contracts are deployed using the first signer/account by default
-  const [owner, addr1, addr2] = await ethers.getSigners();
+  const [owner, addr1, addr2, addr3] = await ethers.getSigners();
 
   const TokenExchangeFactory = await ethers.getContractFactory("TokenExchange");
   const tokenExchangeDeploy = await TokenExchangeFactory.deploy(name, symbol);
@@ -30,7 +30,7 @@ async function deployOneYearTokenExchangeFixture(isTransfer: boolean = false) {
     await tokenExchangeDeploy.transfer(addr1.address, 500);
   }
 
-  return { tokenExchangeDeploy, unlockTime, lockedAmount, owner, addr1, addr2 };
+  return { tokenExchangeDeploy, unlockTime, lockedAmount, owner, addr1, addr2, addr3 };
 }
 
 describe("TokenExchange Contract", function() {
@@ -99,9 +99,14 @@ describe("TokenExchange Contract", function() {
   });
   describe("exchange transaction", function() {
     it("should execute token diposit", async function() {
-      const { tokenExchangeDeploy, owner, addr1, addr2} = await loadFixture(deployOneYearTokenExchangeFixture);
+      const { tokenExchangeDeploy, owner, addr1, addr2, addr3} = await loadFixture(deployOneYearTokenExchangeFixture);   
       await tokenExchangeDeploy.transfer(addr1.address, 500);
+      await tokenExchangeDeploy.transfer(addr2.address, 200);
+      await tokenExchangeDeploy.transfer(addr3.address, 100);
+      await tokenExchangeDeploy.connect(addr1).deposit(100);
+      await tokenExchangeDeploy.connect(addr2).deposit(200);
       const addr1Balance = await tokenExchangeDeploy.balanceOf(addr1.address);
+      console.log("addr1Balance" + addr1Balance);
       expect(addr1Balance).to.equal(400);
       const addr1ExchangeBalance = await tokenExchangeDeploy.exchangeBalanceOf(addr1.address);
       expect(addr1ExchangeBalance).to.equal(100);
@@ -129,21 +134,26 @@ describe("TokenExchange Contract", function() {
       const { tokenExchangeDeploy, owner, addr1, addr2} = await loadFixture(deployOneYearTokenExchangeFixture);
       const startExchangeBalance = await tokenExchangeDeploy.connect(addr1).exchangeBalanceOf(addr1.address);
       const startExchangeTotalDeposit = await tokenExchangeDeploy.connect(addr1).exchangeTotalDeposit();
-
       await tokenExchangeDeploy.connect(addr1).withdraw(100);
-
       const endExchangeBalance = await tokenExchangeDeploy.connect(addr1).exchangeBalanceOf(addr1.address);
       const endExchangeTotalDeposit = await tokenExchangeDeploy.connect(addr1).exchangeTotalDeposit();
       expect(endExchangeBalance).to.equal(startExchangeBalance - BigInt(100));
       expect(endExchangeTotalDeposit).to.equal(startExchangeTotalDeposit - BigInt(100));
     });
     it("should be failed when token balanace is shotage.", async function() {
-      const { tokenExchangeDeploy, owner, addr1, addr2} = await loadFixture(deployOneYearTokenExchangeFixture);
-      expect(1).to.equal(100);
+      const { tokenExchangeDeploy, owner, addr1, addr2, addr3} = await loadFixture(deployOneYearTokenExchangeFixture);   
+      await tokenExchangeDeploy.transfer(addr1.address, 500);
+      await tokenExchangeDeploy.transfer(addr2.address, 200);
+      await tokenExchangeDeploy.transfer(addr3.address, 100);
+      await tokenExchangeDeploy.connect(addr1).deposit(100);
+      await tokenExchangeDeploy.connect(addr2).deposit(200);
+      expect(tokenExchangeDeploy.connect(addr1).withdraw(101))
+      .to.be.revertedWith("An amount greater than your tokenExchange balance!");
     });
     it("should be issue TokenWithdraw event.", async function() {
       const { tokenExchangeDeploy, owner, addr1, addr2} = await loadFixture(deployOneYearTokenExchangeFixture);
-      expect(1).to.equal(100);
+      expect(tokenExchangeDeploy.connect(addr1).withdraw(100))
+      .emit(tokenExchangeDeploy, "TokenWithdraw").withArgs(addr1.address, 100);
     });
   });
 });
